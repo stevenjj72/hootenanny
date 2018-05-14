922f7146b11488623a674a4f6e7f2a27dd9e9aed
Cellington1 <cody.ellington@radiantsolutions.com>
2018-05-14 11:19:36 -0400

    Tracing the SetupOsmApiDb shell to check db status during build

--- scripts/database/SetupOsmApiDB.sh
+++ scripts/database/SetupOsmApiDB.sh
@@ -16,6 +16,8 @@ do_create="true"
 # see if old db osmapi_test exists
 export flag=`psql $AUTH -lqt | cut -d \| -f 1 | grep -w "^ $DB_NAME_OSMAPI \+" | wc -l`
 
+echo "Database Status is $flag"
+
 if [ "$flag" = "1" ]; then
   # DB Exists, check creation date:
   SQL_SCRIPT="CREATE OR REPLACE FUNCTION checkDbTime() RETURNS text LANGUAGE plpgsql AS \$\$ 
@@ -49,23 +51,23 @@ if [ "$flag" = "1" ]; then
 
   if [ $file_date -ge $db_date ] || [ "$1" = "force" ]; then
     # Drop the DB
-    # echo "DROPPING $DB_NAME_OSMAPI"
+    echo "DROPPING $DB_NAME_OSMAPI"
     dropdb $AUTH $DB_NAME_OSMAPI
     do_create="true"
   else
-    # DB exists, and appears to be up to date, do nothing
+   # DB exists, and appears to be up to date, do nothing
     do_create="false"
   fi
 fi
 
 # create the osm apu db from the blank osm api db script
 if [ "$do_create" = "true" ]; then
-  #echo "Creating osm api db"
-  #echo "DB_HOST_OSMAPI: " $DB_HOST_OSMAPI
-  #echo "DB_PORT_OSMAPI: " $DB_PORT_OSMAPI
-  #echo "DB_USER_OSMAPI: " $DB_USER_OSMAPI
-  #echo "PGPASSWORD: " $PGPASSWORD
-  #echo "DB_NAME_OSMAPI: " $DB_NAME_OSMAPI
+  echo "Creating osm api db"
+  echo "DB_HOST_OSMAPI: " $DB_HOST_OSMAPI
+  echo "DB_PORT_OSMAPI: " $DB_PORT_OSMAPI
+  echo "DB_USER_OSMAPI: " $DB_USER_OSMAPI
+  echo "PGPASSWORD: " $PGPASSWORD
+  echo "DB_NAME_OSMAPI: " $DB_NAME_OSMAPI
   createdb $AUTH $DB_NAME_OSMAPI
 
  # psql $AUTH -d $DB_NAME_OSMAPI -f $HOOT_HOME/scripts/database/blank_osmapidb.sql  >& $HOOT_HOME/osmapidb.log
