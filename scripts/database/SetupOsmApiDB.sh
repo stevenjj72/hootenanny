1347eff8e95f9bbe0e3bc0a5ec2df30655751908
Cellington1 <cody.ellington@radiantsolutions.com>
2018-05-16 12:34:37 -0400

    increasing verbose logging

--- scripts/database/SetupOsmApiDB.sh
+++ scripts/database/SetupOsmApiDB.sh
@@ -70,7 +70,7 @@ if [ "$do_create" = "true" ]; then
   echo "DB_NAME_OSMAPI: " $DB_NAME_OSMAPI
   createdb $AUTH $DB_NAME_OSMAPI
 
-  psql $AUTH -d $DB_NAME_OSMAPI -f $HOOT_HOME/scripts/database/blank_osmapidb.sql  >& osmapidb.log
+  psql $AUTH -d $DB_NAME_OSMAPI -f $HOOT_HOME/scripts/database/blank_osmapidb.sql -a  &> osmapidb.log
 
   cat osmapidb.log
 fi
